ca170831d6003a3745731be0b382c64aaec2e33c
abdtrust19991 <abdullah@telenoc.org>
2023-01-06 18:12:19 +0300

    [Report updated]

--- custom_report_layout/__manifest__.py
+++ custom_report_layout/__manifest__.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Header Custom Layout Report",
+    'name': "Custom Report Layout",
     'version': '16.0.1.0.0',
     'summary': 'A module to configure report header and footer',
+    'sequence': -100,
     'description': 'A module to configure report header and footer manually from company form',
     'category': 'All',
     'license': 'LGPL-3',
     'depends': ['base','account'],
-    'author': "Eng.Abdullah Essa",
     'data': [
         'report/base_document_layout.xml',
-        'views/views.xml',
+        #'views/views.xml',
         # "data/report_paperformat.xml",
         # "report/account_report.xml",
         # "views/invoice_report.xml",
